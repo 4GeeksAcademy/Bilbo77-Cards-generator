a390f3b7ebe27d7acb8500fa1aebc2cdf871de77
Bilbo L.R <bilbo.lr@gmail.com>
2024-04-07 09:04:09 +0000

    newCardsGen

--- src/app.js
+++ src/app.js
@@ -2,27 +2,18 @@ import "bootstrap";
 import "./style.css";
 
 window.onload = () => {
-  let generateRandomNumber = () => {
-    let numbers = [
-      "A",
-      "2",
-      "3",
-      "4",
-      "5",
-      "6",
-      "7",
-      "8",
-      "9",
-      "10",
-      "J",
-      "Q",
-      "K"
-    ];
-
-    let suits = ["&heart", "&diamond", "&spade", "&club"];
-
-    let singleNumber = numbers[Math.floor(Math.random() * numbers.length)];
-    let singleSuit = suits[Math.floor(Math.random() * suits.length)];
-  };
-  document.querySelector("#randomNumber").innerHTML = singleNumber;
+  document.getElementById('randomNumber').innerHTML = theNumbers();
+  document.querySelector("#suitsHTML1").innerHTML = theSuits();
+  document.querySelector("#suitsHTML2").innerHTML = theSuits();
 };
+    let theNumbers = () => {
+    let numbers = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
+    let randomNumbers = numbers[Math.floor(Math.random() * numbers.length)];
+    return randomNumbers;
+  };
+
+    let theSuits =  () => {
+    let suits = ["&#9829;", "&#9830;", "&#9824;", "&#9827;"];
+    let randomSuits = suits[Math.floor(Math.random() * suits.length)];
+    return randomSuits;
+  };
