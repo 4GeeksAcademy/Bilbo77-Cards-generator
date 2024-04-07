8ef3e57e17e1b2fb8b6f3aa5bfdbae0b4fb907ad
Bilbo L.R <bilbo.lr@gmail.com>
2024-04-07 06:32:24 +0000

    chao prettier

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  let generateRandomNumber = () => {
+    let numbers = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+    let suits = ["&heart", "&diamond", "&spade", "&club"];
+
+    let singleNumber = numbers[Math.floor(Math.random() * numbers.length)];
+    let singleSuit = suits[Math.floor(Math.random() * suits.length)];
+  };
+  document.querySelector("#randomNumber").innerHTML = singleNumber;
 };
