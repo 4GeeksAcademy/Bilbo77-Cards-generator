2e5baebb76c19e4d342d28795c7ab8603a07aff4
Bilbo L.R <bilbo.lr@gmail.com>
2024-04-07 10:46:24 +0000

    RandomCards

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import "./style.css";
 window.onload = () => {
   document.getElementById('randomNumber').innerHTML = theNumbers();
   document.querySelector("#suitsHTML1").innerHTML = theSuits();
-  document.querySelector("#suitsHTML2").innerHTML = theSuits();
+  document.querySelector("#suitsHTML2").innerHTML = document.querySelector("#suitsHTML1").innerHTML;
 };
     let theNumbers = () => {
     let numbers = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
@@ -14,6 +14,9 @@ window.onload = () => {
 
     let theSuits =  () => {
     let suits = ["&#9829;", "&#9830;", "&#9824;", "&#9827;"];
+    let changeColor = suits[0,1];
     let randomSuits = suits[Math.floor(Math.random() * suits.length)];
     return randomSuits;
   };
+
+  
